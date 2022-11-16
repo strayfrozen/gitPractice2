ade48ac593de9b33a21169de20dc5dad6f4a36ff
Stray <strayfrozen@yahoo.com>
2022-11-16 07:54:28 -0600

    practice

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/db/608e1b2db565001d1d8d91e255a47fa0
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author stray
+ *
+ */
+module gitPractice2 {
+}
---
+++ gitPractice2/src/executable/com/Practice.java
@@ -0,0 +1,11 @@
+package executable.com;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
